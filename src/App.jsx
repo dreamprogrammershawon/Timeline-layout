0c86d1ee6020f0183cb5bdfba00ccc69e0095a40
dreamprogrammershawon <dreamprogrammer252@gmail.com>
2024-06-02 08:41:34 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,159 @@
+import React from 'react'
+import "./App.css"
+
+const App = () => {
+  return (
+    <>
+    <div className="container">
+      <div className="timeline">
+        <ul>
+          <li>
+            <div className="timeline-content-right">
+              <h2 >Stage <span>|</span></h2>
+              <div className="text">
+                <div className="ok">
+                  <img src="../assets/images/ok.png" alt="" />
+               
+                </div>
+                <p className='p c'>Ut enim ad minim veniam, quis nostrud  exercitation ullamco laboris .</p>
+              </div>
+              <div className="text">
+                <div className="ok">
+                  <img src="../assets/images/ok.png" alt="" />
+                 
+                </div>
+                <p className='p c'>Ut enim ad minim veniam, quis nostrud  exercitation ullamco laboris .</p>
+              </div>
+              <div className="text">
+                <div className="ok">
+                  <img src="../assets/images/ok.png" alt="" />
+                </div>
+                <p className='p'>Ut enim ad minim veniam, quis nostrud </p>
+
+              </div>
+              <div className="text">
+                <div className="ok">
+                  <img src="../assets/images/ok.png" alt="" />
+                </div>
+                <p className='p'>Ut enim ad minim veniam, quis nostrud</p>
+
+              </div>
+              <div className="text">
+                <div className="ok">
+                  <img src="../assets/images/ok.png" alt="" />
+                </div>
+                <p className='p'>Ut enim ad minim veniam, quis nostrud</p>
+
+              </div>
+              <div className="text">
+                <div className="Cancel">
+                  <img src="../assets/images/Cancel.png" alt="" />
+                </div>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris .</p>
+
+              </div>
+            </div>
+</li>
+<li>
+            <div className="timeline-content-left">
+              <h2 className='left'>Stage <span>||</span></h2>
+              <div className="text">
+                <div className="Cancel">
+                  <img src="../assets/images/Cancel.png" alt="" />
+                </div>
+                <p className='c'>Ut enim ad minim veniam, quis nostrud  exercitation ullamco laboris .</p>
+
+              </div>
+              <div className="text">
+                <div className="Cancel">
+                  <img src="../assets/images/Cancel.png" alt="" />
+                </div>
+                <p className='c'>Ut enim ad minim veniam, quis nostrud  exercitation ullamco laboris .</p>
+
+              </div>
+              <div className="text">
+                <div className="Cancel">
+                  <img src="../assets/images/Cancel.png" alt="" />
+                </div>
+                <p >Ut enim ad minim veniam, quis nostrud </p>
+
+              </div>
+              <div className="text">
+                <div className="Cancel">
+                  <img src="../assets/images/Cancel.png" alt="" />
+                </div>
+                <p>Ut enim ad minim veniam, quis nostrud</p>
+
+              </div>
+              <div className="text">
+                <div className="Cancel">
+                  <img src="../assets/images/Cancel.png" alt="" />
+                </div>
+                <p >Ut enim ad minim veniam, quis nostrud</p>
+
+              </div>
+              <div className="text">
+                <div className="Cancel">
+                  <img src="../assets/images/Cancel.png" alt="" />
+                </div>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris .</p>
+
+              </div>
+            </div>
+            </li>
+<li>
+            <div className="timeline-content-left ">
+              <h2 >Stage <span> |||</span></h2>
+              <div className="text">
+                <div className="Cancel">
+                  <img src="../assets/images/Cancel.png" alt="" />
+                </div>
+                <p className='c'>Ut enim ad minim veniam, quis nostrud  exercitation ullamco laboris .</p>
+
+              </div>
+              <div className="text">
+                <div className="Cancel">
+                  <img src="../assets/images/Cancel.png" alt="" />
+                </div>
+                <p className='c'>Ut enim ad minim veniam, quis nostrud  exercitation ullamco laboris .</p>
+
+              </div>
+              <div className="text">
+                <div className="Cancel">
+                  <img src="../assets/images/Cancel.png" alt="" />
+                </div>
+                <p >Ut enim ad minim veniam, quis nostrud </p>
+
+              </div>
+              <div className="text">
+                <div className="Cancel">
+                  <img src="../assets/images/Cancel.png" alt="" />
+                </div>
+                <p >Ut enim ad minim veniam, quis nostrud</p>
+
+              </div>
+              <div className="text">
+                <div className="ok">
+                  <img src="../assets/images/Cancel.png" alt="" />
+                </div>
+                <p >Ut enim ad minim veniam, quis nostrud</p>
+
+              </div>
+              <div className="text">
+                <div className="Cancel">
+                  <img src="../assets/images/Cancel.png" alt="" />
+                </div>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris .</p>
+
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  
+    </>
+  )
+}
+
+export default App
